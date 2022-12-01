05c2132a3bef5a345d541344bdfaf9c8266eee4c
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2022-12-01 02:18:55 -0300

    Aplicação do layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import logo from "./assets/img/logo.png"
+import seta_play from "./assets/img/seta_play.png"
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="screen-container">
+      <div className="logo-container">
+        <img src={logo}/>
+        <h1>ZapRecall</h1>
+      </div>
+      <div className="pergunta-fechada">
+        <p>Pergunta1</p>
+        <img src={seta_play}/>
+      </div>
+      <div className="pergunta-fechada">
+        <p>Pergunta2</p>
+        <img src={seta_play}/>
+      </div>
+      <div className="pergunta-fechada">
+        <p>Pergunta3</p>
+        <img src={seta_play}/>
+      </div>
+      <div className="pergunta-fechada">
+        <p>Pergunta4</p>
+        <img src={seta_play}/>
+      </div>
+      <div className="pergunta-fechada">
+        <p>Pergunta5</p>
+        <img src={seta_play}/>
+      </div>
+      <div className="pergunta-fechada">
+        <p>Pergunta6</p>
+        <img src={seta_play}/>
+      </div>
+      <div className="pergunta-fechada">
+        <p>Pergunta7</p>
+        <img src={seta_play}/>
+      </div>
+      ,<div className="pergunta-fechada">
+        <p>Pergunta8</p>
+        <img src={seta_play}/>
+      </div>
+      <div className="footer-concluidos">
+        0/4 CONCLUÍDOS
+      </div>
     </div>
   );
 }
-
-export default App;
